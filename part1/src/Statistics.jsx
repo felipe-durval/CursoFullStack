06111e26cb223d03a1e5df4b57c00500dfe130d3
Felipe <felipe.doprado@hotmail.com>
2026-02-03 11:18:27 -0300

    Exercise 1.11

--- part1/src/Statistics.jsx
+++ part1/src/Statistics.jsx
@@ -13,13 +13,16 @@ const Statistics = ({ good, neutral, bad }) => {
     ) : (     
         <div>
             <h2>Statistics</h2>
-            <StatisticsLine text="good" value={good} />
-            <StatisticsLine text="neutral" value={neutral} />
-            <StatisticsLine text="bad" value={bad} />
-            <StatisticsLine text="all" value={total} />
-            <StatisticsLine text="average" value={average.toFixed(2)} />
-            <StatisticsLine text="positive" value={`${positive.toFixed(2)} %`} />  
-            
+            <table>
+                <tbody>
+                <StatisticsLine text="good" value={good} />
+                <StatisticsLine text="neutral" value={neutral} />
+                <StatisticsLine text="bad" value={bad} />
+                <StatisticsLine text="all" value={total} />
+                <StatisticsLine text="average" value={average.toFixed(2)} />
+                <StatisticsLine text="positive" value={`${positive.toFixed(2)} %`} />  
+                </tbody>  
+            </table>
         </div>
     )
 }   
